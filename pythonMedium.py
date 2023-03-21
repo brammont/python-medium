25795d4bf9be351a313db349211c73ff01783b99
brammont <brayanmazuera1@gmail.com>
2023-03-21 12:42:55 +0800

    creating new branch

--- pythonMedium.py
+++ pythonMedium.py
@@ -1,16 +1,17 @@
-set_a ={'col','mex','bol'}
-set_b ={'pe','bol'}
-# operation sets
-#ad
-set_c = set_a.union(set_b)
-print(set_c) 
-print(set_a | set_b)
-#intersection
-set_c = set_a.intersection(set_b)
-print(set_c) 
-print(set_a | set_b)
-#difference
-set_c = set_a.difference(set_b)
-print(set_c) 
-print(set_a | set_b)
+#list conhension
+days = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+newlist = []
 
+for i in days:
+  if "a" in i:
+    newlist.append(i)
+
+print(newlist) #["martes", "sabado"]
+
+#List Comprehension
+
+days = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+
+newlist = [i for i in days if "a" in i]
+
+print(newlist) # ["martes", "sabado"]
